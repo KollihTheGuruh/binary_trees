d52a6119270aac17c68bb1c990a3932aae8c7fd6
KollihTheGuruh <mmike8449@gmail.com>
2023-08-30 19:42:09 +0300

    binary tree size

--- binary_trees.h
+++ binary_trees.h
@@ -32,5 +32,6 @@ size_t binary_tree_height(const binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif
