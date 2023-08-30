b04d090a786979f4a87f0c52d67d4a01d9e4ac35
KollihTheGuruh <mmike8449@gmail.com>
2023-08-31 00:42:47 +0300

    110

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,44 +1,52 @@
 #include "binary_trees.h"
-#include <stddef.h>
-
 /**
- * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree.
- *
- * @tree: A pointer to the root node of the binary tree.
- *
- * Return: 1 if tree is a valid BST, and 0 otherwise.
+ * find_node -function find node in a tree
+ * @root: root of the tre to evaluate
+ * @node: node to find
+ * Return:  function must return 1 if tree is a valid BST, and 0 otherwise
+ */
+int find_node(binary_tree_t *root, binary_tree_t *node)
+{
+
+	if (root == NULL)
+		return (0);
+	if (node == root)
+		return (1);
+	if (node->n < root->n)
+		return (find_node(root->left, node));
+	if (node->n > root->n)
+		return (find_node(root->right, node));
+	return (0);
+}
+/**
+ * croos_tree - cross the tree checking if each node exist correctly
+ * @root: root node of the tree
+ * @node: current node to evaluate
+ * Return: 1 if is BST0 if no
+ */
+int croos_tree(binary_tree_t *root, binary_tree_t *node)
+{
+	if (root && node)
+	{
+		int aux = 0;
+
+		aux = find_node(root, node);
+		if (node->left)
+			aux &= croos_tree(root, node->left);
+		if (node->right)
+			aux &= croos_tree(root, node->right);
+		return (aux);
+	}
+	return (0);
+}
+/**
+ * binary_tree_is_bst - function that checks if a binary tree is a valid Binary Search Tree
+ * @tree: tre to check
+ * Return: 1 if is bst 0 if not
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
-	return (is_bst_helper(tree, INT_MIN, INT_MAX));
-}
-
-/**
- * is_bst_helper - Checks if a binary tree is a binary search tree.
- *
- * @tree: A pointer to the root node of the binary tree.
- * @low: The lower bound for the current node's value in the BST.
- * @high: The upper bound for the current node's value in the BST.
- *
- * Function is a helper function intended to be used
- * recursively to check whether
- * the binary tree satisfies the properties of
- * a binary search tree within the given range.
- *
- * Return: 1 if the tree is a valid BST within the given range, 0 otherwise.
- */
-int is_bst_helper(const binary_tree_t *tree, int low, int high)
-{
-	if (tree != NULL)
-	{
-		if (tree->n < low || tree->n > high)
-			return (0);
-		return (is_bst_helper(tree->left, low, tree->n - 1) &&
-			is_bst_helper(tree->right, tree->n + 1, high));
-	}
-
-	return (1);
+	return (croos_tree((binary_tree_t *)tree, (binary_tree_t *)tree));
 }
